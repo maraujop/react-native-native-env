eab38fbb7bf8361013a8ec0f1f05d0ce0a589bc4
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2017-09-21 16:50:14 +0200

    Upgrade for RN 0.47 compatibility

--- android/src/main/java/com/xgfe/reactnativeenv/RCTNativeEnvPackage.java
+++ android/src/main/java/com/xgfe/reactnativeenv/RCTNativeEnvPackage.java
@@ -32,11 +32,6 @@ public class RCTNativeEnvPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
